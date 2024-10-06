8c50804b90ba4c1b0b0fcda3ea59d0ef0d91d470
abegailsantos <aegailsantos.1819@gmail.com>
2024-10-06 17:39:24 -0400

    update passing_structs_by_reference_2.c

--- c_basics/passing_structs_by_reference_2.c
+++ c_basics/passing_structs_by_reference_2.c
@@ -10,6 +10,7 @@ and instantiate new structs with values using a function rather than using dot n
 */
 
 #include <stdio.h> 
+#include <stdlib.h>  // Required for malloc and free
 
 /* student struct */ 
 typedef struct student {
@@ -28,7 +29,7 @@ student_t* makeStudent(int id, const char* name, int age) {
     student_t* p_aStudent = (student_t*)malloc(sizeof(student_t));
 
     p_aStudent-> id = id;
-    p_aStudent-> name = id;
+    p_aStudent-> name = name;
     p_aStudent-> age = age;
 
     return p_aStudent;
@@ -58,14 +59,15 @@ int main() {
     printStudent(s2);
 
     // Update student details using a pointer
-    updateStudent(s1, 24, 2008); // Pass pointer to s1 and new values
-    updateStudent(s2, s2->age, 2001); // Pass pointer to s2 and new values
+    updateStudent(s1, 24, 2008); 
+    updateStudent(s2, s2->age, 2001);
 
     // Print new student details
     printf("\nAfter updating:\n");
     printStudent(s1);
     printStudent(s2);
 
+    // Free allocated memory
     free(s1);
     free(s2);
 
