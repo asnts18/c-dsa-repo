2de6b332d2acd8273bbe8967ab627d184f4a3ec1
abegailsantos <aegailsantos.1819@gmail.com>
2024-10-06 17:36:25 -0400

    add passing_structs_by_reference_2.c

---
+++ c_basics/passing_structs_by_reference_2.c
@@ -0,0 +1,73 @@
+/*
+Abegail Santos
+CS 5008 - Fall 2024 
+
+Module 2: The C Programming Language: Intro to C, pointers, and arrays
+Passing Structs by Reference and Use of the Arrow Operator
+
+similar to passing_structs_by_reference.c, this code shows how to update and print structs
+and instantiate new structs with values using a function rather than using dot notation
+*/
+
+#include <stdio.h> 
+
+/* student struct */ 
+typedef struct student {
+    int id;
+    const char* name; // Pointer to student name (string)
+    int age;
+} student_t; 
+
+
+/*  
+ * Function to create pointer to a student_t
+ * Return: pointer to a student_t
+ */ 
+student_t* makeStudent(int id, const char* name, int age) {
+
+    student_t* p_aStudent = (student_t*)malloc(sizeof(student_t));
+
+    p_aStudent-> id = id;
+    p_aStudent-> name = id;
+    p_aStudent-> age = age;
+
+    return p_aStudent;
+}
+
+/* Function to print details of student_t */ 
+void printStudent(student_t* aStudent) {
+    printf("ID: %d, Name: %s, Age: %d\n",
+    aStudent->id, aStudent->name, aStudent->age);
+}
+
+/* Function to update a student_t by reference */ 
+void updateStudent(student_t* aStudent, int age, int id) {
+
+    // Access members of the structure pointed to by aStudent using arrow notation
+    aStudent->age = age;
+    aStudent->id = id;
+}
+
+int main() {
+    // create instances of pointers to student_t
+    student_t* s1 = makeStudent(1008, "Abegail", 23); 
+    student_t* s2 = makeStudent(1001, "Kevin", 25); 
+
+    // Pass pointers to s1 and s2, and print details 
+    printStudent(s1);
+    printStudent(s2);
+
+    // Update student details using a pointer
+    updateStudent(s1, 24, 2008); // Pass pointer to s1 and new values
+    updateStudent(s2, s2->age, 2001); // Pass pointer to s2 and new values
+
+    // Print new student details
+    printf("\nAfter updating:\n");
+    printStudent(s1);
+    printStudent(s2);
+
+    free(s1);
+    free(s2);
+
+    return 0;
+}
